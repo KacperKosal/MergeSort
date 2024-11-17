17a01bcdcb8bd7d94f13127acb0580c2bcae29e1
Kacper Kosal <kacper.kosal@op.pl>
2024-11-17 19:07:01 +0100

    Dodanie Komentarzy

--- MergeSort/MergeSort.cpp
+++ MergeSort/MergeSort.cpp
@@ -3,15 +3,19 @@
 #include "MergeStoreclass.hpp"
 
 int main() {
+    // Tworzymy przykładową tablicę
     std::vector<int> arr = { 38, 27, 43, 3, 9, 82, 10 };
 
+    // Wypisujemy oryginalną tablicę
     std::cout << "Original array: ";
     for (int num : arr) {
         std::cout << num << " ";
     }
 
+    // Sortujemy tablicę
     MergeSort::sort(arr);
 
+    // Wypisujemy posortowaną tablicę
     std::cout << "\nSorted array: ";
     for (int num : arr) {
         std::cout << num << " ";
